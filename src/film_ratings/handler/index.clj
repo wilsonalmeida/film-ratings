cdc42149d5ce1eb442ec6c39d8a629d904d5a08a
Dionysius Wilson Almeida <dionysius.almeida@gmail.com>
2020-02-20 17:36:54 +1100

    disable default anti-forgery-wrapper and use custom middleware to store and check from session cookie "x-csrf-token"

--- src/film_ratings/handler/index.clj
+++ src/film_ratings/handler/index.clj
@@ -3,18 +3,8 @@
             [ataraxy.response :as response]
             [film-ratings.views.index :as views.index]
             [integrant.core :as ig]
-            ;;[ataraxy.handler :as handler]
             ))
-
-;; (defmethod handler/sync-default ::ok [{[_ body] :ataraxy/result}]
-;;   (let [result (response/->response body)]
-;;     (pprint (format "Result : %s" result))
-;;     (-> result
-;;         (resp/header "anti-forgery-token" (force *anti-forgery-token*))
-;;         (resp/status 200)))
-;;   )
 
 (defmethod ig/init-key :film-ratings.handler.index/index [_ options]
   (fn [_]
-    ;;fn [{[_] :ataraxy/result}]
     [::response/ok (views.index/list-options)]))
--- src/film_ratings/handler/middleware.clj
+++ src/film_ratings/handler/middleware.clj
@@ -1,18 +1,30 @@
 (ns film-ratings.handler.middleware
-  (:require [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
+  (:require [ring.middleware.anti-forgery :refer [*anti-forgery-token*
+                                                  wrap-anti-forgery]]
             [integrant.core :as ig]
             [clojure.pprint :refer [pprint]]
             [ring.util.response :as resp]))
 
-(defn- wrap-anti-forgery [handler]
-  (pprint (format "Handler is %s" handler))
+(defn- wrap-anti-forgery-field [handler]
+  ;; (pprint (format "Handler is %s\n" handler))
   (fn [request]
-    (pprint (format "Request is %s" request))
+    ;; (pprint (format "Request is %s\n" request))
     (let [response  (handler request)]
-      (print (format "Response from handler: %s" response))
+      ;; (print (format "Response from handler: %s\n" response))
       (->  response
-           (resp/header "X-CSRF-Token"  (force *anti-forgery-token*))))))
+           (resp/set-cookie "x-csrf-token"  (force *anti-forgery-token*)
+                            {:same-site :strict :path "/" :max-age 3600})))))
 
 (defmethod ig/init-key  ::anti-forgery-field [this config]
-  (print (format "key is %s: " this))
-  #(wrap-anti-forgery %))
+  ;; (print (format "In anti-forgery-field: Key is: %s\n" this))
+  #(wrap-anti-forgery-field %))
+
+(defn- my-custom-token [request]
+  ;; (pprint (format "In my-custom-token: Request is %s\n" request))
+  (let [token (get-in request [:cookies "x-csrf-token" :value])]
+    ;; (pprint (format "Token is %s\n" token))
+    token))
+
+(defmethod ig/init-key ::my-wrap-anti-forgery [this config]
+  ;; (print (format "In my-wrap-anti-forgery: Key is %s\n" this))
+  #(wrap-anti-forgery % {:read-token my-custom-token}))
